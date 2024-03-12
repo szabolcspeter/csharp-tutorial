65f23a7508c3f9b39c5c71a909776e54b55d1982
Szabolcs József Péter <Szabolcs-Jozsef.Peter@t-systems.com>
2024-03-12 13:06:37 +0100

    2d array

---
+++ DataStructures/2DArray/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _2DArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tomb2d = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            Console.WriteLine("Bejárás for ciklusokkal: ");
+            for (int i=0; i<tomb2d.GetLength(0); i++)
+            {
+                for (int j=0; j<tomb2d.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", tomb2d[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Bejárás foreach ciklussal: ");
+            foreach (var elem in tomb2d)
+            {
+                Console.Write("{0} ", elem);
+            }
+        }
+    }
+}
---
+++ DataStructures/2DJaggedArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _2DJaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var jagged = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5, 6 },
+                new int[] { 7, 8, 9 },
+            };
+
+            foreach (var sor in jagged)
+            {
+                foreach (var oszlop in sor)
+                {
+                    Console.Write("{0} ", oszlop);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
